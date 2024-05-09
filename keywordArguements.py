95acfa6205c7cacecadc85441935a6af95c7c287
Agashthemonkey <yug.chaudhari@education.nsw.gov.au>
2024-05-09 10:13:37 +1000

    09/05/24

---
+++ keywordArguements.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name ):
+    print(f'hi {first_name}{last_name}!')
+    print('welcome aboard')
+
+
+print('start')
+greet_user("john", 'smith')
+calc_cost(total=50, shipping=5, discount=0.1)
+print('finish')
---
+++ parameters.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name ):
+    print(f'hi {first_name}{last_name}!')
+    print('welcome aboard')
+
+
+print('start')
+greet_user("john", " smith")
+greet_user('mary',' smith')
+print('finish')
